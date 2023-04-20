73b2c3698bcbd5fba84b7963eebecc0a14095b3b
mateu52 <wuoelte@gmail.com>
2023-04-20 20:01:53 +0200

    pobranie dni i wartosci z json

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 const Day = document.getElementsByClassName('div.Day');
 console.log('hello');
 
-const DaysList = document.querySelector('.Daysweek')
-DaysList.style.background = "lightblue";
+
+const ChartElements = document.querySelector("chartEl");
+const DaysList = document.querySelector('div.daysList');
+//ChartElements.background = "beige";
+//DaysList.style.background = "lightblue";
+//DaysList.style.display = "flex;"
 let Lista = ""
 fetch('/data.json')
     .then(res => res.json())
@@ -14,9 +18,16 @@ fetch('/data.json')
             let Dzien = document.createElement('p');
             Dzien.classList.add('Day');
             Dzien.innerHTML = post.day;
+            
+            Dzien.style.height = (post.amount+ "px");
             DaysList.appendChild(Dzien);
-
+            
+            let Chart  = document.createElement('p');
+            Chart.classList.add("chart");
+            
             // DaysList.style.height = (post.amount+ "px");
+            //ChartElements.appendChild(Chart);
+
             //document.getElementsByClassName('.DaysList').appendChild(DaysList);
 
         })
